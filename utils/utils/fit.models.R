e4d3b51057e61b6bc67f665dae7ae16ceef84c79
PaulaKuper <77276549+PaulaKuper@users.noreply.github.com>
2024-08-22 11:29:56 +0200

    Delete utils directory

--- utils/utils/fit.models.R
+++
@@ -1,6 +0,0 @@
-fitModels <- function(formula, att_scale, data){
-  M <- rma(yi=.g, sei=.g_se, scale= formula, 
-           data=data, method= "REML", att= att_scale,
-           test="knha", control= list(optimizer="BFGS")) 
-  return(M)
-}
